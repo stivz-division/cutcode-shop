bc2ef294dadc24e2bc5a7ee41fd9cf1f3f61d3a7
Alexandr <stivz-division@mail.ru>
2022-10-16 05:43:39 +1100

    update templates

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,18 @@ export default defineConfig({
         host: '0.0.0.0',
         hmr: {
             host: 'localhost'
+        },
+        watch: {
+            usePolling: true
         }
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/sass/main.sass',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
